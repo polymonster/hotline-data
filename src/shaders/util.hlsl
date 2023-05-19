5dcb68a31268af7c12070a03bf4ea7bf584ab609
polymonster <alexandercdixon@gmail.com>
2023-05-19 22:27:20 +0100

    - dynamic cubemap wip

--- src/shaders/util.hlsl
+++ src/shaders/util.hlsl
@@ -31,6 +31,50 @@ void cs_mip_chain_texture2d(uint2 did: SV_DispatchThreadID) {
         level_up += rw_texture[read][did.xy * 2];
     }
     
-    //rw_texture[write][did.xy] = float4(1.0, 0.0, 1.0, 1.0);
     rw_texture[write][did.xy] = level_up / 9.0;
+}
+
+//
+// clear cubemap background
+//
+
+struct vs_input_2d_texcoord {
+    float2 position : POSITION;
+    float2 texcoord: TEXCOORD;
+};
+
+struct vs_output_ndc {
+    float4 position: SV_POSITION0;
+    float2 ndc: TEXCOORD;
+};
+
+vs_output_ndc vs_ndc(vs_input_2d_texcoord input) {
+    vs_output_ndc output;
+    output.position = float4(input.position.xy, 0.0, 1.0);
+    output.ndc = input.position.xy;
+    return output;
+}
+
+cbuffer cubemap_clear_constants : register(b0) {
+    float4x4 inverse_wvp;
+};
+
+TextureCube cubemap : register(t0);
+SamplerState sampler_wrap_linear : register(s0); 
+
+float4 ps_cubemap_clear(vs_output_ndc input) : SV_Target {
+    // unproject ray
+    float2 ndc = input.ndc;
+    float4 near = float4(ndc.x, ndc.y, 0.0, 1.0);
+    float4 far = float4(ndc.x, ndc.y, 1.0, 1.0);
+    
+    float4 wnear = mul(inverse_wvp, near);
+    wnear /= wnear.w;
+    
+    float4 wfar = mul(inverse_wvp, far);
+    wfar /= wfar.w;
+
+    float3 rd = normalize(wfar.xyz - wnear.xyz);
+
+    return cubemap.Sample(sampler_wrap_linear, rd);
 }
